a55ce3a2da4ce6f83d65124011c1f315bf388683
KingFjL <fanjianglong1997@hotmail.com>
2017-08-18 09:06:16 +0800

    Update Thor.py

--- Codingame/Thor.py
+++ Codingame/Thor.py
@@ -1,4 +1,4 @@
-import sys 
+mport sys 
 import math
 
 original_x = int(raw_input)
@@ -6,16 +6,22 @@ original_y = int(raw_input)
 aim_x = int(raw_input)
 aim_y = int(raw_input)
 
-if original_x < aim_x:
+
+while 1:
+    dir = ""
+
+    if original_x < aim_x:
     oringinal_x += 1
-    d_1 = 'E'
-else:
+    dir += 'E'
+    elif original_x > aim_x:
     original_x -= 1
-    d_1 = 'W'
-if original_y < aim_y:
+    dir = 'W'
+
+    if original_y < aim_y:
     original_y += 1
-    d_2 = 'N'
-else:
+    dir = 'N'
+    else:
     original_y -= 1
-    d_2 = 'S'
-print 'The direction is:' + d_1 + d_2
+    dir = 'S'
+
+    print 'The direction is:' + dir
