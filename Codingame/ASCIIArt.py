c02bc963104defd42015eea40ed0a69581d369c8
KingFjL <fanjianglong1997@hotmail.com>
2017-08-22 09:57:18 +0800

    Update ASCIIArt.py

--- Codingame/ASCIIArt.py
+++ Codingame/ASCIIArt.py
@@ -9,7 +9,8 @@ H = int(raw_input())
 T = list(raw_input())
 
 for i in range(len(T)):
-    if T[i].isalpha():
+    #isalpha() 方法检测字符串是否只由字母组成
+    if T[i].isalpha():   
         T[i] = T[i].upper()
     else:
         T[i] = '['
@@ -21,6 +22,11 @@ ans = ""
 for i in range(H):
     for c in T:
         n = (ord(c) - ord('A'))*L
+        """
+        ord()函数是chr()函数（对于8位的ASCII字符串）或unichr()函数（对于Unicode对象）的配对函数，
+        它以一个字符（长度为1的字符串）作为参数，返回对应的ASCII数值，或者Unicode数值，如果所给的
+        Unicode字符超出了你的Python定义范围，则会引发一个TypeError的异常。
+        """
         ans += set[i][n:n+L]
     ans += "\n"
 print ans
