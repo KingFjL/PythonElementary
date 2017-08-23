cc5b920ff416ad588589981d66c2eea7187724e9
KingFjL <fanjianglong1997@hotmail.com>
2017-08-23 09:20:31 +0800

    Create TheDescent.py

---
+++ Codingame/Easy/TheDescent.py
@@ -0,0 +1,18 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+
+# game loop
+while 1:
+    max = 0
+    imax = 0
+    for i in xrange(8):
+        mountain_h = int(raw_input()) # represents the height of one mountain, from 9 to 0. Mountain heights are provided from left to right.
+        if mountain_h > max:
+            max = mountain_h
+            imax = i
+
+    print imax
