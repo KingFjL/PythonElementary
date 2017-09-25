4ad033dc27b65e4b97b355b162c02414cceeebba
龙 <fanjianglong1997@hotmail.com>
2017-09-25 10:27:41 +0800

    Solved by Python 2

--- Codingame/Easy/05_MARS LANDER - EPISODE 1.py
+++ Codingame/Easy/05_MARS LANDER - EPISODE 1.py
@@ -1,3 +1,8 @@
+'''
+    The goal for your program is to safely land the "Mars Lander" shuttle, the landing ship which contains the Opportunity rover. 
+    Mars Lander is guided by a program, and right now the failure rate for landing on the NASA simulator is unacceptable.
+'''
+
 import sys
 import math
 
