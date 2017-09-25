68d8f06e73a576657b6a9b3da70f7881356bf3dd
龙 <fanjianglong1997@hotmail.com>
2017-09-25 10:17:10 +0800

    Update 01_Enemy.py

--- Codingame/Easy/01_Enemy.py
+++ Codingame/Easy/01_Enemy.py
@@ -1,11 +1,16 @@
+'''
+   消灭附近的敌人
+   Your program must destroy the enemy ships by shooting the closest enemy on each turn.
+'''
+
 import sys
 import math
 
 
-enemy_1 = raw_input
-dist_1 = int(raw_input)
-enemy_2 = raw_input
-dist_2 = int(raw_input)
+enemy_1 = raw_input() # name of enemy 1
+dist_1 = int(raw_input()) # distance to enemy 1
+enemy_2 = raw_input() # name of enemy 2
+dist_2 = int(raw_input()) # distance to enemy 2
 
 
 while True:
