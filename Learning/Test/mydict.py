54f5bd8d88095e530b374b468949a8906cd49179
龙 <fanjianglong1997@hotmail.com>
2017-10-16 17:56:50 +0800

    Create mydict.py

---
+++ Learning/Test/mydict.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+
+'''
+新建一个Dict类，以便用来测试
+'''
+
+
+__metaclass__ = type
+
+class Dict(dict):
+	
+	def __init__(self, **kw):
+		super(Dict, self).__init__(**kw)
+		
+	def __getattr__(self, key):
+		try:
+			return self[key]
+		except KeyError:
+			raise AttributeError(r"'Dict' object has  \
+				no attribute '%s'" % key)
+
+	def __setattr__(self, key, value):
+		self[key] = value
+		
