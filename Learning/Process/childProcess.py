f36fe4a16054c7f40d1bded7419438804f3c2996
龙 <fanjianglong1997@hotmail.com>
2017-10-16 18:07:12 +0800

    Create childProcess.py

---
+++ Learning/Process/childProcess.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+
+
+'''
+启动子进程并等待其结束
+'''
+
+from multiprocessing import Process
+import os
+
+
+def run_proc(name):
+	print 'Run child process %s (%s)...' % (name, os.getpid())
+
+if __name__ == '__main__':
+	print "Parent process %s." % os.getpid()
+	p = Process(target=run_proc, args=('test',))
+	print 'Process will start.'
+	p.start()
+	p.join()
+	print 'Process end'
+
+
+
+
+
+'''
+运行结果：
+Parent process 5080.
+Process will start.
+Run child process test <6016>...
+Process end
+'''
