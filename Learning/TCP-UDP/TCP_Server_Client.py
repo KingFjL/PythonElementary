01a6b35439bb4e29fb51f664002007c656a57c6c
龙 <fanjianglong1997@hotmail.com>
2017-10-18 17:01:46 +0800

    Create TCP_Server_Client.py

---
+++ Learning/TCP-UDP/TCP_Server_Client.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+
+
+import socket,threading
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#建立连接:
+s.connect(('127.0.0.1', 9999))
+#接受欢迎信息:
+print s.recv(1024)
+for data in ['Michael', 'Tracy', 'Sarah']:
+	#发送数据:
+	s.send(data)
+	print s.recv(1024)
+s.send('exit')
+s.close()
